a884d997bac44cc9a08bf33f4e2c183ae3c6c137
NicolasEstebanCuervo <rojascuervo942@gmail.com>
2023-12-13 16:30:26 -0500

    News v5

--- src/Pages/EconomyPage/index.tsx
+++ src/Pages/EconomyPage/index.tsx
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 import NavBarComponent from "../../Components/NavBar";
 import Footer from "../../Components/Footer";
 import HeaderComponent from "../../Components/ComponentsEconomyPage/Header";
+import EconomyNew from "../../Components/ComponentsEconomyPage/EconomyNew";
 import WeeklyNews from "../../Components/ComponentsEconomyPage/WeeklyNews";
 import MultiNews from "../../Components/ComponentsEconomyPage/MultiNews";
 import MultiEconomy from "../../Components/ComponentsEconomyPage/MultiEconomy";
@@ -12,6 +13,7 @@ export default function EconomyPage() {
             <NavBarComponent />
             <SubContainerMain>
                 <HeaderComponent />
+                <EconomyNew />
                 <WeeklyNews/>
                 <MultiNews />
                 <MultiEconomy />
